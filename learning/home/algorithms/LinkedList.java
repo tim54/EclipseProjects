0f020c35f03e22e572ecb7ddf478f974110550d5
Kovalevsky Artem-RCFT46 <Artem.Kovalevsky@motorolasolutions.com>
2016-11-10 22:07:39 +0300

    10-Nov
    Change-Id: Ieaba5ef1d8b009abaf5f2f4b59ac38968a580bac

--- learning/home/algorithms/LinkedList.java
+++ learning/home/algorithms/LinkedList.java
@@ -1,50 +1,113 @@
 package home.algorithms;
 
-class Node {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-	Node next = null;
-	int data;
+import tmp.LinkedList.Node;
 
-	public Node(int d) {
-		data = d;
+public class LinkedList<Item> implements Iterable<Item>{
+	
+	private Node first;
+	private Node end;
+	private int n;
+	
+	private class Node {
+		private Item item;
+		private Node next;
 	}
-
-	void appendToTail(int d) {
-		Node end = new Node(d);
-		Node n = this;
-		while (n.next != null) {
-			n = n.next;
-		}
-		n. next = end;
+	
+	public LinkedList() {
+		first = null;
+		end = null;
+		n = 0;
 	}
-
-	Node deleteNode(Node head, int d) {
-		Node n = head;
-
-		if (n.data == d) {
-			return head.next; /* moved head */
+	
+	public boolean isEmpty() {
+		return first == null;
+	}
+	
+	public int size() {
+		return n;
+	}
+	
+	public void add(Item item){
+		Node newNode = new Node();
+		newNode.item = item;
+		
+		if (end != null) {
+			end.next = newNode;
+			end = newNode;
+		} else {
+			first = end = newNode;
 		}
-
-		while (n.next != null) {
-			if (n.next.data == d) {
-				n.next = n.next.next;
-				return head; /* head didn't change */
+		
+	}
+	
+	public Iterator<Item> iterator() {
+		return new ListIterator();
+	}
+	
+	private class ListIterator implements Iterator<Item> {
+		private Node current = first;
+		
+		public boolean hasNext() { return current != null; }
+		public void remove() { throw new UnsupportedOperationException(); }
+		
+		public Item next() {
+			if (!hasNext()) throw new NoSuchElementException();
+			Item item = current.item;
+			current = current.next;
+			return item;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		
+		StringBuilder str = new StringBuilder();
+		
+		Node focusNode = first;
+		str.append("List is: ");
+		while (focusNode.next != null) {
+			str.append(focusNode.item.toString() + " ");
+			focusNode = focusNode.next;
+		}
+		str.append(focusNode.item.toString() + ";");
+		return str.toString();
+	}
+	
+	public void revertList(){
+		Node focusNode = first;
+		Node previousNode = null;
+		Node tmp = new Node();
+		
+		while(focusNode.next != null){
+			previousNode = focusNode;
+			focusNode = focusNode.next;
+			
+			if (previousNode == first){
+				tmp.next = previousNode;
+				previousNode.next = null;
+			} else {
+				previousNode.next = tmp.next;
+				tmp.next = focusNode;
 			}
-			n = n.next;
+			first = end;
 		}
-		return head;
 	}
-}
-
-public class LinkedList {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Node myNode = new Node(1);
-		myNode.appendToTail(2);
-		myNode.appendToTail(3);
-		myNode.deleteNode(myNode, 3);
+		LinkedList<Integer> intList = new LinkedList<>();
+		intList.add(1);
+		intList.add(2);
+		intList.add(3);
+		intList.add(4);
+		
+		System.out.println(intList.toString());
+		intList.revertList();
+		System.out.println(intList.toString());
 	}
 
 }
